c54439accb4b4977bd71d22ae2208d90c584ef4c
Furkan Doğu <furkandogu2018@gmail.com>
2023-11-03 11:25:23 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const main = document.querySelector(".bgDiv")
+const btnClick = document.querySelector(".btn-click")
+const btnOver = document.querySelector(".btn-over")
+const colorInput = document.getElementById("colorInput")
+const colorText = document.getElementById("colorText")
+
+// btnClick.onclick = function () {
+//     main.style.backgroundColor = "red"
+// }
+
+// btnOver.onmouseover = function () {
+//     main.style.backgroundColor = "yellow"
+// }
